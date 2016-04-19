dd5e7062e4b79dd174953e5e405c09dc57f02af1
Andrew Smith-Mui <arsm800@gmail.com>
2016-04-19 16:17:12 -0400

    Updated order of scripts in html so that svg variable defined in script.js appears in html upon page reload.

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ var dataset = {
     {source: 4, target: 6}
   ]
 };
+
+var h = 300;
+var w = 500;
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("height", h)
+            .attr("width", w);
+
+//Initialize a force layout.//
+var force = d3.layout.force()
+                     .nodes(dataset.nodes)
+                     .links(dataset.edges)
+                     .size([w, h])
+                     .linkDistance([250])
+                     .charge([-100])
+                     .start();
+
+//Create a line for each edge in dataset.//
+var edges = svg.selectAll("line")
+               .data(dataset.edges)
+               .enter()
+               .append("line")
+               .style("stroke", "#ccc")
+               .style("stroke-width", 1);
+
+//Create a circle for each node.//
+var nodes = svg.selectAll("circle")
+               .data(dataset.nodes)
+               .enter()
+               .append("circle")
+               .attr("r", 20)
+               .style("fill", "#ccc");
+              //  .call(force.drag);     //Enable drag and drop interaction.//
