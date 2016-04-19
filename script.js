fa56bffef4b4bb7f3d2ce27da2dd15959ab7b796
Andrew Smith-Mui <arsm800@gmail.com>
2016-04-19 14:50:18 -0400

    Linked D3 to html.  Created dataset with nodes and edges.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Connected!");
+var dataset = {
+  nodes: [
+    {text: "name"},
+    {text: "Contact"},
+    {text: "email"},
+    {text: "gitHub"},
+    {text: "projects"},
+    {text: "license plates"},
+    {text: "electric car maps"},
+    {text: "vision"}
+  ],
+  edges: [
+    {source: 0, target: 1},
+    {source: 0, target: 4},
+    {source: 0, target: 7},
+    {source: 1, target: 2},
+    {source: 1, target: 3},
+    {source: 4, target: 5},
+    {source: 4, target: 6}
+  ]
+};
