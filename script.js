3927f6e288e9e5b115c338a9eb007f83a777fd40
Andrew Smith-Mui <arsm800@gmail.com>
2016-04-20 12:19:37 -0400

    Fixed text and lines.  Created g element and appended to circles, so both circles and text are elements of g element, which is en element on top of the svg.  Fixed lines by pointing x2 and y2 to target instead of source.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var dataset = {
   nodes: [
-    {text: "name", r: 200, color: "#000066"},
+    {text: "Andrew Smith-Mui", r: 200, color: "#000066"},
     {text: "Contact", r: 100, color: "#00FF99"},
     {text: "email", r: 50, color: "#22A4FE"},
     {text: "gitHub", r: 50, color: "#22A4FE"},
@@ -49,27 +49,35 @@ var edges = svg.selectAll("line")
 var nodes = svg.selectAll("circle")
                .data(dataset.nodes)
                .enter()
-               .append("circle")
-               .attr("r", function(d) {
+               .append("g");    //In order to append other elements to shapes in and svg throughd d3, you must create and append a "g" element on top of the svg.  You can append it to each instance of circle as seen here.  Once you do this, you can append the text.  You are appending both the circle and the text to the g element.  See the DOM in the browser to confirm.//
+
+nodes.append("circle")
+     .attr("r", function(d) {
                  return d.r;              //Appends individual radii specified in dataset to nodes.//
                })
-              //  .append("text")
-               .attr("text", function(d) {
-                 return d.text;
-               })
-               .style("fill", function(d) {
+     .style("fill", function(d) {
                  return d.color;
                })
-               .call(force.drag);     //Enable drag and drop interaction.//
+     .call(force.drag);     //Enable drag and drop interaction.//
 
-//Specifies what happens when force layout ticks.//
+nodes.append("text")
+     .text(function(d) {
+            return d.text;
+          })
+     .style("fill", "black");
+
+
+//Specifies what happens when force layout ticks.  It also seems to be where you set locations of shapes in svg.//
 force.on("tick", function() {
 
   edges.attr("x1", function(d) { return d.source.x; })
        .attr("y1", function(d) { return d.source.y; })
-       .attr("x2", function(d) { return d.source.x; })
-       .attr("y2", function(d) { return d.source.y; });
+       .attr("x2", function(d) { return d.target.x; })
+       .attr("y2", function(d) { return d.target.y; });
 
-  nodes.attr("cx", function(d) { return d.x; })
+  nodes.select("circle").attr("cx", function(d) { return d.x; })
        .attr("cy", function(d) { return d.y; });
+
+  nodes.select("text").attr("x", function(d) { return d.x; })
+       .attr("y", function(d) { return d.y; });
 });
